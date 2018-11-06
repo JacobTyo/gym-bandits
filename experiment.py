22ad96ea987f64d30c06b2c3a5c11da6f0eda92b
Jabberwockyll <jabyrd@moreheadstate.edu>
2018-11-05 19:38:05 -0500

    experiment.py now runs multiple algos and plots them together

--- experiment.py
+++ experiment.py
@@ -8,13 +8,42 @@ from ucb import Ucb
 from delayed_ucb import Delayed_Ucb
 
 
-def main():
+def run(args, alg):
 
+    horizon = args.horizon
+    results = []
+
+    for run in range(args.repetitions):
+        # Setup
+        env = gym.make(args.gym + "-v0")
+        env.reset()
+
+        # New algorithms go here
+        agent = None
+        if alg == "ucb":
+            agent = Ucb(env.action_space.n, args)
+        elif alg == "delayed_ucb":
+            agent = Delayed_Ucb(env.action_space.n, args)
+
+        # Experiment
+        action = agent.play(None, non_anon_reward=[])
+        info = None
+        for step in range(horizon):
+            observation, reward, done, info = env.step(action)
+            action = agent.play(reward, non_anon_reward=info["non_anon_reward"])
+        results.append(info)
+
+    expected_rewards = np.asarray([info["expected_reward"] for info in results])
+    mean_expected_rewards = np.mean(expected_rewards, axis=0)
+    std_expected_rewards = np.std(expected_rewards, axis=0)
+    return {"mean": mean_expected_rewards, "std": std_expected_rewards}
+
+def main():
     parser = argparse.ArgumentParser(description='DAAF bandits experiment')
     parser.add_argument('--horizon', type=int, help='length of experiment')
     parser.add_argument('--repetitions', type=int, help='Number of times to run experiment')
     parser.add_argument('--ucb_delta', type=float, help='ucb error probability')
-    parser.add_argument('--alg', type=str, choices=["ucb", "delayed_ucb"], help='bandit algorithm to run')
+    #parser.add_argument('--alg', type=str, choices=["ucb", "delayed_ucb"], help='bandit algorithm to run')
     parser.add_argument('--gym', type=str, choices=['BanditTenArmedRandomFixed',
                 'BanditTenArmedRandomRandom',
                 'BanditTenArmedGaussian',
@@ -32,39 +61,24 @@ def main():
                 'AnonymousDelayedBanditTenArmedStochasticDelayStochasticReward2'], help='bandit environment')
     args = parser.parse_args()
     horizon = args.horizon
-    results = []
 
-    for run in range(args.repetitions):
-        # Setup
-        env = gym.make(args.gym + "-v0")
-        env.reset()
+    algs = ["ucb", "delayed_ucb"]
+    results = {}
+    for alg in algs:
+        results[alg] = run(args, alg)
 
-        # New algorithms go here
-        agent = None
-        if args.alg == "ucb":
-            agent = Ucb(env.action_space.n, args)
-        elif args.alg == "delayed_ucb":
-            agent = Delayed_Ucb(env.action_space.n, args)
-
-        # Experiment
-        action = agent.play(None, non_anon_reward=[])
-        info = None
-        for step in range(horizon):
-            observation, reward, done, info = env.step(action)
-            action = agent.play(reward, non_anon_reward=info["non_anon_reward"])
-        results.append(info)
-
-    max_mean = results[0]["optimal_mean"]
-    expected_rewards = np.asarray([info["expected_reward"] for info in results])
-    mean_expected_rewards = np.mean(expected_rewards, axis=0)
-    std_expected_rewards = np.std(expected_rewards, axis=0)
+    tempenv = gym.make(args.gym + "-v0")
+    tempenv.reset()
+    max_mean = tempenv.step(0)[-1]["optimal_mean"]
     # Results
-    # print("mean reward: {}".format(np.mean(info["reward"])))
-    total_regret = (max_mean * horizon) - np.sum(mean_expected_rewards)
-    print("mean regret per step: {}".format(total_regret / horizon))
-    cumregret = np.cumsum(max_mean - mean_expected_rewards)
-    plt.plot(cumregret)
-    plt.fill_between(np.arange(horizon), cumregret - std_expected_rewards, cumregret + std_expected_rewards, alpha=0.5)
+    for alg in algs:
+        mean = results[alg]["mean"]
+        std = results[alg]["std"]
+        total_regret = (max_mean * horizon) - np.sum(mean)
+        print("{} mean regret per step: {}".format(alg, total_regret / horizon))
+        cumregret = np.cumsum(max_mean - mean)
+        plt.semilogy(cumregret)
+        plt.fill_between(np.arange(horizon), cumregret - std, cumregret + std, alpha=0.5)
     plt.xlabel("Step")
     plt.ylabel("Cumulative Regret")
     plt.show()
