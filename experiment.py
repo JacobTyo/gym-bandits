1a0d9491bf23c6478b23cd97285ea63e64559b88
Jabberwockyll <jabyrd@moreheadstate.edu>
2018-11-05 19:15:28 -0500

    added repetitions to experiment and confidence band to plot

--- experiment.py
+++ experiment.py
@@ -12,6 +12,7 @@ def main():
 
     parser = argparse.ArgumentParser(description='DAAF bandits experiment')
     parser.add_argument('--horizon', type=int, help='length of experiment')
+    parser.add_argument('--repetitions', type=int, help='Number of times to run experiment')
     parser.add_argument('--ucb_delta', type=float, help='ucb error probability')
     parser.add_argument('--alg', type=str, choices=["ucb", "delayed_ucb"], help='bandit algorithm to run')
     parser.add_argument('--gym', type=str, choices=['BanditTenArmedRandomFixed',
@@ -31,31 +32,39 @@ def main():
                 'AnonymousDelayedBanditTenArmedStochasticDelayStochasticReward2'], help='bandit environment')
     args = parser.parse_args()
     horizon = args.horizon
+    results = []
 
-    # Setup
-    env = gym.make(args.gym + "-v0")
-    env.reset()
+    for run in range(args.repetitions):
+        # Setup
+        env = gym.make(args.gym + "-v0")
+        env.reset()
 
-    # New algorithms go here
-    agent = None
-    if args.alg == "ucb":
-        agent = Ucb(env.action_space.n, args)
-    elif args.alg == "delayed_ucb":
-        agent = Delayed_Ucb(env.action_space.n, args)
+        # New algorithms go here
+        agent = None
+        if args.alg == "ucb":
+            agent = Ucb(env.action_space.n, args)
+        elif args.alg == "delayed_ucb":
+            agent = Delayed_Ucb(env.action_space.n, args)
 
-    # Experiment
-    action = agent.play(None, non_anon_reward=[])
-    info = None
-    for step in range(horizon):
-        observation, reward, done, info = env.step(action)
-        action = agent.play(reward, non_anon_reward=info["non_anon_reward"])
+        # Experiment
+        action = agent.play(None, non_anon_reward=[])
+        info = None
+        for step in range(horizon):
+            observation, reward, done, info = env.step(action)
+            action = agent.play(reward, non_anon_reward=info["non_anon_reward"])
+        results.append(info)
 
+    max_mean = results[0]["optimal_mean"]
+    expected_rewards = np.asarray([info["expected_reward"] for info in results])
+    mean_expected_rewards = np.mean(expected_rewards, axis=0)
+    std_expected_rewards = np.std(expected_rewards, axis=0)
     # Results
     # print("mean reward: {}".format(np.mean(info["reward"])))
-    max_mean = info["optimal_mean"]
-    total_regret = (max_mean * horizon) - np.sum(info["expected_reward"])
-    print("mean regret: {}".format(total_regret / horizon))
-    plt.plot(np.cumsum(max_mean - np.asarray(info["expected_reward"])))
+    total_regret = (max_mean * horizon) - np.sum(mean_expected_rewards)
+    print("mean regret per step: {}".format(total_regret / horizon))
+    cumregret = np.cumsum(max_mean - mean_expected_rewards)
+    plt.plot(cumregret)
+    plt.fill_between(np.arange(horizon), cumregret - std_expected_rewards, cumregret + std_expected_rewards, alpha=0.5)
     plt.xlabel("Step")
     plt.ylabel("Cumulative Regret")
     plt.show()
@@ -66,13 +75,6 @@ def main():
     # plt.ylabel("reward")
     # plt.show()
     # plt.figure()
-# plt.plot(total_rewards)
-# plt.show()
-# plt.figure()
-# plt.plot(np.arange(10000), cumulative_reward_list[:10000], np.arange(10000), optimal_reward_list[:10000])
-# plt.show()
-# print(eliminated_arms)
-# print(cumulative_reward)
 
 if __name__ == "__main__":
     main()
