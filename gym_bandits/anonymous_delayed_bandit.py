46478d05d1ed34b8ad8f37aea661daa705284797
Jabberwockyll <jabyrd@moreheadstate.edu>
2018-12-12 14:25:25 -0500

    Changed AdaBanditOutliers to fit with experiment 3 and 4 (non-optimal outlier)

--- gym_bandits/anonymous_delayed_bandit.py
+++ gym_bandits/anonymous_delayed_bandit.py
@@ -398,35 +398,29 @@ class AdaBanditsOutliers(AnonymousDelayedBanditEnv):
     def __init__(self, bandits=10):
         p_dist = [1 for i in range(bandits)]
 
-        r_dist = [functools.partial(np.random.normal, 0.01, 1, 1),
-                  functools.partial(np.random.normal, 0.1, 1, 1),
-                  functools.partial(np.random.normal, 0.2, 1, 1),
-                  functools.partial(np.random.normal, 0.3, 1, 1),
-                  functools.partial(np.random.normal, 0.4, 1, 1),
-                  functools.partial(np.random.normal, 0.5, 1, 1),
-                  functools.partial(np.random.normal, 0.7, 1, 1),
-                  functools.partial(np.random.normal, 0.8, 1, 1),
-                  functools.partial(np.random.normal, 0.9, 1, 1),
-                  functools.partial(np.random.normal, 0.99, 1, 1)]
+        self.means = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
-        self.means = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.7, 0.8, 0.9, 0.99]
+        d_dist = [functools.partial(np.random.poisson, 1, 1),
+                  functools.partial(np.random.poisson, 2, 1),
+                  functools.partial(np.random.poisson, 3, 1),
+                  functools.partial(np.random.poisson, 4, 1),
+                  functools.partial(np.random.poisson, 5, 1),
+                  functools.partial(np.random.poisson, 6, 1),
+                  functools.partial(np.random.poisson, 7, 1),
+                  functools.partial(np.random.poisson, 8, 1),
+                  functools.partial(np.random.poisson, 9, 1),
+                  functools.partial(np.random.poisson, 100, 1)]
 
-        d_dist = [functools.partial(np.random.normal, 8, 1, 1),
-                  functools.partial(np.random.normal, 3, 1, 1),
-                  functools.partial(np.random.normal, 4, 1, 1),
-                  functools.partial(np.random.normal, 7, 1, 1),
-                  functools.partial(np.random.normal, 2, 1, 1),
-                  functools.partial(np.random.normal, 1, 1, 1),
-                  functools.partial(np.random.normal, 7, 1, 1),
-                  functools.partial(np.random.normal, 9, 1, 1),
-                  functools.partial(np.random.normal, 6, 1, 1),
-                  functools.partial(np.random.normal, 5, 1, 1)]
+        while self.means[-1] == 1.0:
+            random.shuffle(self.means)
 
-        # c = list(zip(r_dist, self.means, d_dist))
-        #
-        # random.shuffle(c)
-        #
-        # r_dist, self.means, d_dist = zip(*c)
+        r_dist = [functools.partial(np.random.normal, mean, 1, 1) for mean in self.means]
+
+        c = list(zip(r_dist, self.means, d_dist))
+
+        random.shuffle(c)
+
+        r_dist, self.means, d_dist = zip(*c)
 
         AnonymousDelayedBanditEnv.__init__(self, p_dist=p_dist, r_dist=r_dist, d_dist=d_dist)
 
